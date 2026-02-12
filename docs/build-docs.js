b88587925d52e136e83941ee650f295de20d4684
lazier334 <lazier334@lazier334.com>
2026-02-13 01:12:17 +0800

    page: = 优化打包

--- docs/build-docs.js
+++ docs/build-docs.js
@@ -15,6 +15,8 @@ const copyConfig = {
 };
 const handlerConfig = {
     mdFlag: '${MD_TEXT}',
+    urlSuffixFlag: '${URL_SUFFIX}',
+    urlSuffix: '.html',
     index: fs.readFileSync(path.join(_dirname(import.meta.url), 'index.html'), { encoding: 'utf8' }),
     outdir: path.join(_dirname(import.meta.url), '../html'),
     scanDirs: [
@@ -30,7 +32,7 @@ const handlerConfig = {
             const newPath = filepath + '.html';
             const md = fs.readFileSync(filepath, { encoding: 'utf8' });
             const encoder = new TextEncoder();
-            let html = this.index.replaceAll(this.mdFlag, encoder.encode(md))
+            let html = this.index.replaceAll(this.mdFlag, encoder.encode(md)).replaceAll(this.urlSuffixFlag, this.urlSuffix);
             fs.writeFileSync(newPath, html);
             fs.unlinkSync(filepath);
             console.log('已处理md文件:', filepath, '->', newPath);
